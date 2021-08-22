89adb23f17770d806307b687d85e05cd49f507ef
booldook <booldook@gmail.com>
2021-08-22 14:50:29 +0900

    03.weather

---
+++ js/03.weather.js
@@ -0,0 +1,7 @@
+/**
+ * url: https://openweathermap.com
+ * appid: 02efdd64bdc14b279bc91d9247db4722 (꼭 본인거)
+ * daily: https://api.openweathermap.org/data/2.5/weather
+ * 5days: https://api.openweathermap.org/data/2.5/forecast
+ * test: https://api.openweathermap.org/data/2.5/weather?appid=02efdd64bdc14b279bc91d9247db4722&units=metric&lat=37.555275&lon=126.936924
+ */
